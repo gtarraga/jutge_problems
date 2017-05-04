a50f2ac23ac7882f28eb12df6d52df00022c3c9a
teleportex <guillem.ramirez.miranda@est.fib.upc.edu>
2017-05-04 23:26:24 +0200

    Added BrainFuck problems!

---
+++ c++ problems w brainfuck/README.md
@@ -0,0 +1,2 @@
+# C++ PROBLEMS MADE WITH BRAINFUCK
+This directory has some jutge.org problems solved using brainfuck interpret. ** :)! **
---
+++ c++ problems w brainfuck/sum_of_three_integers/sum_three_integers.cc
@@ -0,0 +1,49 @@
+//Adaptation of the brainfuck_int to solve jutge.org problems
+
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+const int MAX_MEMORY = 30000; //#The number of 8 bit memory cells that the original brainfuck had
+
+int main(){
+    vector<char> memory(MAX_MEMORY, 0);
+    string instructions = ">,[>,]<[<]"; //bf code here
+    stack<int> pos_buc;
+    int pointer = 0;
+
+    for(int i = 0; i<instructions.size(); ++i){
+        switch (instructions[i]) {
+            case '+':
+                memory[pointer]++;
+                break;
+            case '-':
+                memory[pointer]--;
+                break;
+            case '>':
+                pointer++;
+                break;
+            case '<':
+                pointer--;
+                break;
+            case '[':
+                pos_buc.push(i);
+                break;
+            case ']':
+                if(memory[pointer] == 0) pos_buc.pop();
+                else i = pos_buc.top();
+                break;
+            case '.':
+                cout<<memory[pointer];
+                break;
+            case ',':
+                char a;
+                cin>>a;
+                memory[pointer] = a;
+                break;
+
+        }
+    }
+    cout<<endl;
+}
